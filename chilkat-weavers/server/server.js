42bd5be9dca16eb8b41f1e69658884d5a05fa83b
Jaime Hunt <hunt.healing@gmail.com>
2021-12-04 09:23:30 -0800

    coming back to this later, looking at other options

--- chilkat-weavers/server/server.js
+++ chilkat-weavers/server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const usersRoutes = require('./routes/users');
 const photosRoutes = require('./routes/photos');
 const postRoutes = require('./routes/posts');
@@ -7,6 +10,52 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8082;
 
 app.use(cors());
+app.use(session({
+    secret: "secret",
+    resave: false ,
+    saveUninitialized: true ,
+}))
+app.use(passport.initialize()) // init passport on every route call
+app.use(passport.session())    //allow passport to use "express-session"
+
+
+//Get the GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET from Google Developer Console
+const GOOGLE_CLIENT_ID = "3184701-tn6finlstvgcgvte2381.apps.googleusercontent.com"
+const GOOGLE_CLIENT_SECRET = "XyLuTLHX6Ov_93IP"
+
+authUser = (request, accessToken, refreshToken, profile, done) => {
+    return done(null, email);
+  }
+
+//Use "GoogleStrategy" as the Authentication Strategy
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/auth/google/callback",
+    passReqToCallback   : true
+  }, authUser));
+
+
+passport.serializeUser( (user, done) => { 
+    console.log(`\n--------> Serialize User:`)
+    console.log(user)
+     // The USER object is the "authenticated user" from the done() in authUser function.
+     // serializeUser() will attach this user to "req.session.passport.user.{user}", so that it is tied to the session object for each session.  
+
+    done(null, user)
+} )
+
+
+passport.deserializeUser((user, done) => {
+        console.log("\n--------- Deserialized User:")
+        console.log(user)
+        // This is the {user} that was saved in req.session.passport.user.{user} in the serializationUser()
+        // deserializeUser will attach this {user} to the "req.user.{user}", so that it can be used anywhere in the App.
+
+        done (null, user)
+}) 
+
+
 app.use(express.json());
 app.use(express.static("./public"));
 
